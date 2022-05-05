442c0b629e14c0801f25b9fcf55713147bb10c15
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2022-05-05 18:28:45 -0400

    Modification of cmake configuration for build on Linux

---
+++ scripts/download_dependencies.sh
@@ -0,0 +1,9 @@
+echo Downloading packages required for to build dependecies of the project
+sudo apt install libxrandr-dev
+sudo apt install libudev-dev
+sudo apt install libfreetype-dev
+sudo apt install libopenal-dev
+sudo apt install libvorbis-dev
+sudo apt install libflac-dev
+sudo apt install libxinerama-dev
+sudo apt install libxcursor-dev
